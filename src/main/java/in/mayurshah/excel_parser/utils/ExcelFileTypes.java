1a04264760ce942360fdd2a99c0a71c8b8903f67
Mayur Shah <mayur@kagrana.com>
2016-04-19 11:05:50 +0530

    adding list() in ExcelFileTypes

--- src/main/java/in/mayurshah/excel_parser/utils/ExcelFileTypes.java
+++ src/main/java/in/mayurshah/excel_parser/utils/ExcelFileTypes.java
@@ -3,4 +3,14 @@ package in.mayurshah.excel_parser.utils;
 public enum ExcelFileTypes {
 	XLSX,XLS,CSV;
 
+	public static String[] list() {
+		ExcelFileTypes[] fileTypes = values();
+	    String[] strFileTypes = new String[fileTypes.length];
+
+	    for (int i = 0; i < fileTypes.length; i++) {
+	        strFileTypes[i] = fileTypes[i].name();
+	    }
+
+	    return strFileTypes;
+	}
 }
